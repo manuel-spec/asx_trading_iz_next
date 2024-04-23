68f0b3dce76305605f8e867e757dbdf7ff9e2d9b
manuel-spec <amaniase084@gmail.com>
2024-04-23 09:56:26 -0700

    send message done

--- src/components/Modal/SupportWindow.tsx
+++ src/components/Modal/SupportWindow.tsx
@@ -1,8 +1,26 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 import support from '../../images/support.svg';
 const SupportWindow = () => {
   const [msg, setMsg] = useState('');
+  const [sentFrom, setSentFrom] = useState('');
+  const [sentTo, setSentTo] = useState('');
+  // useEffect(() => {
+  //   axios.get('http://localhost:9000/api/chat/', {});
+  // }, []);
 
+  const sendMessage = () => {
+    setSentFrom(localStorage.getItem('id'));
+    setSentTo('65dc9dcb42524480b3d04c8a');
+    setMsg(msg);
+    console.log(msg);
+    axios.post('http://localhost:9000/api/chat/', {
+      sent_from: sentFrom,
+      sent_to: sentTo,
+      message: msg,
+    });
+  };
   return (
     <div className="flex flex-col justify-center w-full bg-black fixed h-full top-0 border-slate-800 transition  duration-500 ease-in">
       <div className="flex flex-col justify-center absolute top-0 left-30">
@@ -27,7 +45,10 @@ const SupportWindow = () => {
             value={msg}
             onChange={(e) => setMsg(e.target.value)}
           />
-          <button className="rounded-xl px-4 py-2 bg-[#2850E7] text-white">
+          <button
+            className="rounded-xl px-4 py-2 bg-[#2850E7] text-white"
+            onClick={() => sendMessage()}
+          >
             Send
           </button>
         </div>
